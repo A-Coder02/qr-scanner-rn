be6dad5eb5fa6f5352ae1bb7d5bb7dbcb4cf58df
Arbaj Ansari <arbaj897ansari@gmail.com>
2024-12-13 21:04:42 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import ScanScreen from './ScanScreen'
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <ScanScreen/>
+    </View>
+  )
+}
+
+export default App
